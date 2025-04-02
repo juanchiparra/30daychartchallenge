95d4cfd9676a7cf5a7b75e9b2626938f71b64ab9
Juanchi Parra <juanchiparra@gmail.com>
2025-04-01 21:49:55 -0400

    Create treemap.js

---
+++ 2025/V01/treemap.js
@@ -0,0 +1,110 @@
+export const data = {
+    name: "Diseases",
+    children: [
+        {
+            name: "Non-communicable diseases",
+            children: [
+                {
+                    name: "Cardiovascular diseases",
+                    value: 19414853.09,
+                    percent: 29.81
+                },
+                {
+                    name: "Cancers",
+                    value: 9833234.39,
+                    percent: 15.10
+                },
+                {
+                    name: "Chronic respiratory\ndiseases",
+                    value: 4414181.58,
+                    percent: 6.78
+                },
+                {
+                    name: "Diabetes and kidney\ndiseases",
+                    value: 3195033.81,
+                    percent: 4.91
+                },
+                {
+                    name: "Neurological\ndisorders",
+                    value: 2607635.06,
+                    percent: 4.00
+                },
+                {
+                    name: "Digestive\ndiseases",
+                    value: 2516332.30,
+                    percent: 3.86
+                },
+                {
+                    name: "Others",
+                    value: 1198125.91,
+                    percent: 1.84
+                }
+            ]
+        },
+        {
+            name: "Communicable diseases",
+            children: [
+                {
+                    name: "COVID-19",
+                    value: 7887553.95,
+                    percent: 12.11
+                },
+                {
+                    name: "Respiratory\ninfections",
+                    value: 3365944.54,
+                    percent: 5.17
+                },
+                {
+                    name: "Maternal and neonatal\ndisorders",
+                    value: 2022687.23,
+                    percent: 3.11
+                },
+                {
+                    name: "Enteric\ninfections",
+                    value: 1336219.85,
+                    percent: 2.05
+                },
+                {
+                    name: "NTDs",
+                    value: 847472.27,
+                    percent: 1.30
+                },
+                {
+                    name: "HIV/AIDS",
+                    value: 718079.21,
+                    percent: 1.10
+                }
+            ]
+        },
+        {
+            name: "Injuries",
+            children: [
+                {
+                    name: "Unintentional\ninjuries",
+                    value: 1817195.28,
+                    percent: 2.79
+                },
+                {
+                    name: "Transport\ninjuries",
+                    value: 1274143.19,
+                    percent: 1.96
+                },
+                {
+                    name: "Self-harm and\ninterpersonal violence",
+                    value: 1252359.27,
+                    percent: 1.92
+                }
+            ]
+        },
+        {
+            name: "Others",
+            children: [
+                {
+                    name: "Others",
+                    value:  1429305.11,
+                    percent: 2.19
+                }
+            ]
+        }
+    ]
+};
