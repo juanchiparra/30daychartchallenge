5776ebe919d9eb55e66c9da55e8c82c7f02e2f40
Juanchi Parra <juanchiparra@gmail.com>
2025-04-02 20:51:01 -0400

    Create slope.js

---
+++ 2025/V02/slope.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        type: "Non-communicable diseases",
+        name: "Cardiovascular diseases",
+        value: 19414853.09,
+        value2: 18870288.97,
+        percent: 29.8,
+        percent2: 33.2
+    },
+    {
+        type: "Non-communicable diseases",
+        name: "Cancers",
+        value: 9833234.39,
+        value2: 9520747.13,
+        percent: 15.1,
+        percent2: 16.7
+    },
+    {
+        type: "Non-communicable diseases",
+        name: "Chronic respiratory diseases",
+        value: 4414181.58,
+        value2: 4294555.56,
+        percent: 6.8,
+        percent2: 7.5
+    },
+    {
+        type: "Non-communicable diseases",
+        name: "Diabetes and kidney diseases",
+        value: 3195033.81,
+        value2: 3063927.38,
+        percent: 4.9,
+        percent2: 5.4
+    },
+    {
+        type: "Non-communicable diseases",
+        name: "Digestive diseases",
+        value: 2516332.3,
+        value2: 2490701.31,
+        percent: 3.9,
+        percent2: 4.4
+    },
+    {
+        type: "Communicable diseases",
+        name: "Respiratory infections and tuberculosis",
+        value: 11253498.49,
+        value2: 3770187.57,
+        percent: 17.3,
+        percent2: 6.6
+    }
+];
