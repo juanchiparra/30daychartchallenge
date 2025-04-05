0945dbe3addf9b61230d8423bb7d4821f15e5312
Juanchi Parra <juanchiparra@gmail.com>
2025-04-04 23:47:33 -0400

    Create proportional.js

---
+++ 2025/V04/proportional.js
@@ -0,0 +1,38 @@
+export const data = [
+    [
+        { gender: "women", value: 91 },
+        { gender: "men", value: 100 },
+        { label: "Health" },
+        { description: "Gender equality in three health related\naspects: health status, health behaviour\nand access to health services" }
+    ],
+    [
+        { gender: "women", value: 87.7 },
+        { gender: "men", value: 100 },
+        { label: "Money" },
+        { description: "Gender inequalities in access to\nfinancial resources and women's\nand men's economic situation" }
+    ],
+    [
+        { gender: "women", value: 77 },
+        { gender: "men", value: 100 },
+        { label: "Work" },
+        { description: "The extent to which women and men\ncan benefit from equal access to\nemployment and good working conditions" }
+    ],
+    [
+        { gender: "women", value: 68.4 },
+        { gender: "men", value: 100 },
+        { label: "Time" },
+        { description: "Gender inequalities in allocation of\ntime spent doing care and domestic\nwork and social activities" }
+    ],
+    [
+        { gender: "women", value: 66.1 },
+        { gender: "men", value: 100 },
+        { label: "Knowledge" },
+        { description: "Gender inequalities in educational attainment,\nparticipation in education and training over\nthe life course and gender segregation" }
+    ],
+    [
+        { gender: "women", value: 57.1 },
+        { gender: "men", value: 100 },
+        { label: "Power" },
+        { description: "Gender equality in decision-making\npositions across the political, economic\nand social spheres" }
+    ]
+];
