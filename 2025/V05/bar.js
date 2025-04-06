75cdbdd2b392029500814667f8d936ad328ed54f
Juanchi Parra <juanchiparra@gmail.com>
2025-04-05 22:48:41 -0400

    Create bar.js

---
+++ 2025/V05/bar.js
@@ -0,0 +1,12 @@
+export const data = [
+    { state: "California", score: 30 },
+    { state: "Texas", score: 21 },
+    { state: "Florida", score: 13 },
+    { state: "Colorado", score: 9 },
+    { state: "New York", score: 9 },
+    { state: "Pennsylvania", score: 9 },
+    { state: "Illinois", score: 8 },
+    { state: "Washington", score: 8 },
+    { state: "North Carolina", score: 7 },
+    { state: "Kentucky", score: 6 }
+];
