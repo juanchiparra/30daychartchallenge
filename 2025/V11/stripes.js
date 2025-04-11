a880bef44a0f0c0201db1d7c22284da190dcded5
Juanchi Parra <juanchiparra@gmail.com>
2025-04-11 16:00:11 -0400

    Create stripes.js

---
+++ 2025/V11/stripes.js
@@ -0,0 +1,810 @@
+export const data = [
+    {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1925,
+    Average: 0.08015577,
+    Lower: -0.032653686,
+    Upper: 0.19296521
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1926,
+    Average: 0.23974274,
+    Lower: 0.13145065,
+    Upper: 0.34803483
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1927,
+    Average: 0.13318415,
+    Lower: 0.030099992,
+    Upper: 0.23626828
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1928,
+    Average: 0.15583193,
+    Lower: 0.044860873,
+    Upper: 0.266803
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1929,
+    Average: -0.030145284,
+    Lower: -0.14056227,
+    Upper: 0.08027173
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1930,
+    Average: 0.18549232,
+    Lower: 0.07188631,
+    Upper: 0.29909837
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1931,
+    Average: 0.25890008,
+    Lower: 0.14960614,
+    Upper: 0.368194
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1932,
+    Average: 0.21683604,
+    Lower: 0.11034257,
+    Upper: 0.3233295
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1933,
+    Average: 0.039953325,
+    Lower: -0.06480264,
+    Upper: 0.14470932
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1934,
+    Average: 0.1879609,
+    Lower: 0.0882938,
+    Upper: 0.287628
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1935,
+    Average: 0.15623851,
+    Lower: 0.058800403,
+    Upper: 0.25367662
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1936,
+    Average: 0.1927768,
+    Lower: 0.09877849,
+    Upper: 0.2867751
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1937,
+    Average: 0.3430988,
+    Lower: 0.24253897,
+    Upper: 0.44365862
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1938,
+    Average: 0.350097,
+    Lower: 0.251994,
+    Upper: 0.44820002
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1939,
+    Average: 0.32150054,
+    Lower: 0.21559305,
+    Upper: 0.42740804
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1940,
+    Average: 0.43823355,
+    Lower: 0.32034805,
+    Upper: 0.5561191
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1941,
+    Average: 0.40042704,
+    Lower: 0.20004383,
+    Upper: 0.6008103
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1942,
+    Average: 0.36370382,
+    Lower: 0.16708532,
+    Upper: 0.5603223
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1943,
+    Average: 0.36871934,
+    Lower: 0.16271463,
+    Upper: 0.574724
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1944,
+    Average: 0.5064029,
+    Lower: 0.30780292,
+    Upper: 0.70500284
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1945,
+    Average: 0.4053861,
+    Lower: 0.20501484,
+    Upper: 0.60575736
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1946,
+    Average: 0.24348314,
+    Lower: 0.09634671,
+    Upper: 0.39061958
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1947,
+    Average: 0.27109215,
+    Lower: 0.13050728,
+    Upper: 0.411677
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1948,
+    Average: 0.23763648,
+    Lower: 0.10316438,
+    Upper: 0.3721086
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1949,
+    Average: 0.21849552,
+    Lower: 0.10822029,
+    Upper: 0.3287708
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1950,
+    Average: 0.13567594,
+    Lower: 0.029640768,
+    Upper: 0.24171112
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1951,
+    Average: 0.3011438,
+    Lower: 0.21194752,
+    Upper: 0.39034006
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1952,
+    Average: 0.3776523,
+    Lower: 0.27936175,
+    Upper: 0.47594282
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1953,
+    Average: 0.4399285,
+    Lower: 0.34176815,
+    Upper: 0.53808886
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1954,
+    Average: 0.24554752,
+    Lower: 0.15379502,
+    Upper: 0.33730003
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1955,
+    Average: 0.1649878,
+    Lower: 0.07786778,
+    Upper: 0.25210783
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1956,
+    Average: 0.09913213,
+    Lower: 0.023172084,
+    Upper: 0.17509216
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1957,
+    Average: 0.32696283,
+    Lower: 0.26172915,
+    Upper: 0.39219654
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1958,
+    Average: 0.3446652,
+    Lower: 0.27922317,
+    Upper: 0.4101072
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1959,
+    Average: 0.31429294,
+    Lower: 0.25193402,
+    Upper: 0.37665185
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1960,
+    Average: 0.24683817,
+    Lower: 0.18816018,
+    Upper: 0.30551615
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1961,
+    Average: 0.34229797,
+    Lower: 0.2915179,
+    Upper: 0.39307806
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1962,
+    Average: 0.29825503,
+    Lower: 0.24498852,
+    Upper: 0.35152155
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1963,
+    Average: 0.32548714,
+    Lower: 0.27171797,
+    Upper: 0.3792563
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1964,
+    Average: 0.056436215,
+    Lower: 0.01280323,
+    Upper: 0.10006917
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1965,
+    Average: 0.15787727,
+    Lower: 0.10868705,
+    Upper: 0.20706749
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1966,
+    Average: 0.21340014,
+    Lower: 0.16388856,
+    Upper: 0.2629117
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1967,
+    Average: 0.24475846,
+    Lower: 0.20164095,
+    Upper: 0.28787595
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1968,
+    Average: 0.19365019,
+    Lower: 0.14902225,
+    Upper: 0.23827812
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1969,
+    Average: 0.33091152,
+    Lower: 0.29040638,
+    Upper: 0.37141663
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1970,
+    Average: 0.27723366,
+    Lower: 0.2362451,
+    Upper: 0.31822222
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1971,
+    Average: 0.1564087,
+    Lower: 0.117825516,
+    Upper: 0.19499189
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1972,
+    Average: 0.26850644,
+    Lower: 0.23061424,
+    Upper: 0.3063986
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1973,
+    Average: 0.4122479,
+    Lower: 0.37577677,
+    Upper: 0.44871905
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1974,
+    Average: 0.18977118,
+    Lower: 0.1520721,
+    Upper: 0.22747026
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1975,
+    Average: 0.25154352,
+    Lower: 0.21099263,
+    Upper: 0.2920944
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1976,
+    Average: 0.14646085,
+    Lower: 0.106437124,
+    Upper: 0.18648459
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1977,
+    Average: 0.46538624,
+    Lower: 0.42235443,
+    Upper: 0.5084181
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1978,
+    Average: 0.3675537,
+    Lower: 0.32770112,
+    Upper: 0.4074063
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1979,
+    Average: 0.45315588,
+    Lower: 0.42465636,
+    Upper: 0.4816554
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1980,
+    Average: 0.5583698,
+    Lower: 0.5251017,
+    Upper: 0.59163785
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1981,
+    Average: 0.6123098,
+    Lower: 0.581689,
+    Upper: 0.64293057
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1982,
+    Average: 0.39656603,
+    Lower: 0.35718912,
+    Upper: 0.43594295
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1983,
+    Average: 0.5861076,
+    Lower: 0.5503421,
+    Upper: 0.62187314
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1984,
+    Average: 0.41029125,
+    Lower: 0.37383792,
+    Upper: 0.44674462
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1985,
+    Average: 0.41202748,
+    Lower: 0.3779612,
+    Upper: 0.4460938
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1986,
+    Average: 0.45798472,
+    Lower: 0.42670575,
+    Upper: 0.4892637
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1987,
+    Average: 0.60532415,
+    Lower: 0.5744833,
+    Upper: 0.636165
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1988,
+    Average: 0.6444495,
+    Lower: 0.60933304,
+    Upper: 0.6795659
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1989,
+    Average: 0.5415481,
+    Lower: 0.5067962,
+    Upper: 0.5762999
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1990,
+    Average: 0.7228801,
+    Lower: 0.68686646,
+    Upper: 0.7588938
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1991,
+    Average: 0.7011943,
+    Lower: 0.6663339,
+    Upper: 0.73605466
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1992,
+    Average: 0.48719457,
+    Lower: 0.45317987,
+    Upper: 0.5212093
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1993,
+    Average: 0.52800494,
+    Lower: 0.4905145,
+    Upper: 0.5654954
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1994,
+    Average: 0.59584755,
+    Lower: 0.5607107,
+    Upper: 0.63098437
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1995,
+    Average: 0.7391639,
+    Lower: 0.70595354,
+    Upper: 0.7723743
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1996,
+    Average: 0.6389871,
+    Lower: 0.60547775,
+    Upper: 0.6724965
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1997,
+    Average: 0.7846063,
+    Lower: 0.7523886,
+    Upper: 0.81682396
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1998,
+    Average: 0.93963945,
+    Lower: 0.90536654,
+    Upper: 0.9739124
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 1999,
+    Average: 0.6867827,
+    Lower: 0.65513253,
+    Upper: 0.71843296
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2000,
+    Average: 0.6933825,
+    Lower: 0.6605256,
+    Upper: 0.72623944
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2001,
+    Average: 0.8515781,
+    Lower: 0.8203661,
+    Upper: 0.88279015
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2002,
+    Average: 0.9057643,
+    Lower: 0.87507963,
+    Upper: 0.93644893
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2003,
+    Average: 0.9064679,
+    Lower: 0.8735404,
+    Upper: 0.9393955
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2004,
+    Average: 0.82966846,
+    Lower: 0.7966361,
+    Upper: 0.8627009
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2005,
+    Average: 0.96916026,
+    Lower: 0.93800306,
+    Upper: 1.0003175
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2006,
+    Average: 0.93485045,
+    Lower: 0.90427077,
+    Upper: 0.96543014
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2007,
+    Average: 0.9539991,
+    Lower: 0.9236509,
+    Upper: 0.9843472
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2008,
+    Average: 0.8279476,
+    Lower: 0.7949551,
+    Upper: 0.8609401
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2009,
+    Average: 0.9590794,
+    Lower: 0.92755336,
+    Upper: 0.9906054
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2010,
+    Average: 1.0426692,
+    Lower: 1.0113736,
+    Upper: 1.0739646
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2011,
+    Average: 0.8999955,
+    Lower: 0.86829895,
+    Upper: 0.93169206
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2012,
+    Average: 0.9399048,
+    Lower: 0.907153,
+    Upper: 0.9726566
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2013,
+    Average: 0.9858731,
+    Lower: 0.9507816,
+    Upper: 1.0209646
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2014,
+    Average: 1.0351694,
+    Lower: 1.0012026,
+    Upper: 1.0691361
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2015,
+    Average: 1.1874121,
+    Lower: 1.1535848,
+    Upper: 1.2212394
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2016,
+    Average: 1.2952248,
+    Lower: 1.2640612,
+    Upper: 1.3263884
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2017,
+    Average: 1.207472,
+    Lower: 1.1770725,
+    Upper: 1.2378715
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2018,
+    Average: 1.1249518,
+    Lower: 1.0933498,
+    Upper: 1.1565537
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2019,
+    Average: 1.2533703,
+    Lower: 1.219085,
+    Upper: 1.2876556
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2020,
+    Average: 1.2852182,
+    Lower: 1.2505958,
+    Upper: 1.3198407
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2021,
+    Average: 1.1242033,
+    Lower: 1.0877554,
+    Upper: 1.1606511
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2022,
+    Average: 1.1636031,
+    Lower: 1.1275554,
+    Upper: 1.1996508
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2023,
+    Average: 1.4626105,
+    Lower: 1.4239484,
+    Upper: 1.5012726
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2024,
+    Average: 1.5430979,
+    Lower: 1.5038866,
+    Upper: 1.5823091
+  },
+  {
+    Entity: "World",
+    Code: "OWID_WRL",
+    Year: 2025,
+    Average: 1.6256422,
+    Lower: 1.371569,
+    Upper: 1.8797154
+  }
+]
