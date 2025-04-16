58f4c3b3c316d725fb80c471c08c65a0dc96634e
Juanchi Parra <juanchiparra@gmail.com>
2025-04-15 23:05:19 -0400

    Add V12

---
+++ 2025/V12/voronoi.js
@@ -0,0 +1,16 @@
+export const data = [
+    { breed: "Yorkshire Terrier", male: 23158, female: 17063 },
+    { breed: "Shih Tzu*", male: 29342, female: 22312 },
+    { breed: "Chihuahua*", male: 20577, female: 19057 },
+    { breed: "Maltese", male: 11943, female: 8912 },
+    { breed: "Labrador Retriever*", male: 20426, female: 20760 }, 
+    { breed: "American Pit Bull*", male: 15083, female: 15319 },
+    { breed: "Pomeranian", male: 8016, female: 5968 },
+    { breed: "Poodle*", male: 7715, female: 5725 },
+    { breed: "Havanese", male: 6699, female: 5566 },
+    { breed: "French\nBulldog", male: 6276, female: 4563 },
+    { breed: "Golden\nRetriever", male: 6262, female: 4628 },
+    { breed: "Goldendoodle", male: 5986, female: 4129 },
+    { breed: "Beagle", male: 5513, female: 4665 },
+    { breed: "German\nShepherd", male: 5512, female: 4614 }
+];
