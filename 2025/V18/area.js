9b8343f2fded9197168796a6d7b0ebf4ed07d607
Juanchi Parra <juanchiparra@gmail.com>
2025-04-20 17:30:51 -0400

    Add V18

---
+++ 2025/V18/area.js
@@ -0,0 +1,19 @@
+export const data = [
+  { name: "Gali. Costa | 79 hm3", value: -94, fill: 0.94, line: 0.94, dotted: 0.87 },
+  { name: "Cant. Occ. | 46 hm3", value: -93, fill: 0.93, line: 1, dotted: 0.94 },
+  { name: "Cant. Or. | 73 hm3", value: -86, fill: 0.86, line: 0.94, dotted: 0.84 },
+  { name: "CI P. Vasco | 21 hm3", value: -90, fill: 0.90, line: 1, dotted: 0.88 },
+  { name: "Miño-Sil | 362 hm3", value: -94, fill: 0.94, line: 0.94, dotted: 0.86 },
+  { name: "Duero | 2.908 hm3", value: -87, fill: 0.87, line: 0.94, dotted: 0.79 },
+  { name: "Ebro | 4.447 hm3", value: -75, fill: 0.75, line: 0.9, dotted: 0.77 },
+  { name: "CI Cat. | 677 hm3", value: -36, fill: 0.36, line: 0.96, dotted: 0.78 },
+  { name: "Tajo | 5.788 hm3", value: -64, fill: 0.64, line: 0.70, dotted: 0.57 },
+  { name: "Guadiana | 9.538 hm3", value: -49, fill: 0.49, line: 0.94, dotted: 0.55 },
+  { name: "Júcar | 2.698 hm3", value: -51, fill: 0.51, line: 0.62, dotted: 0.43 },
+  { name: "Guadalquivir | 7.969 hm3", value: -44, fill: 0.44, line: 0.94, dotted: 0.54 },
+  { name: "M. And. | 1.174 hm3", value: -31, fill: 0.31, line: 0.78, dotted: 0.52 },
+  { name: "Segura | 1.134 hm3", value: -23, fill: 0.23, line: 0.73, dotted: 0.32 },
+  { name: "Tinto | 229 hm3", value: -84, fill: 0.84, line: 0.99, dotted: 0.80 },
+  { name: "Guadalete | 1.651 hm3", value: -28, fill: 0.28, line: 0.96, dotted: 0.60 }
+];
+
