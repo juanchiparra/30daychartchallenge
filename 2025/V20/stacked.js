3c70306e5021a7d684e34d55073feb79048c5838
Juanchi Parra <juanchiparra@gmail.com>
2025-04-20 17:31:08 -0400

    Add V20

---
+++ 2025/V20/stacked.js
@@ -0,0 +1,8 @@
+export const data = [
+    { year: "1950", Urban: 29.6, Rural: 70.4},
+    { year: "1970", Urban: 36.6, Rural: 63.4} ,
+    { year: "1990", Urban: 43, Rural: 57 },
+    { year: "2015", Urban: 53.9, Rural: 46.1 },
+    { year: "2030*", Urban: 60.4, Rural: 39.6 },
+    { year: "2050*", Urban: 68.4, Rural: 31.6 },
+];
